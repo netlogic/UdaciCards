63078bcf918ece8e3630e97aad0eb5fa435b9f92
Bret <Bret@Netlogic.com>
2017-09-20 20:51:16 -0400

    building add deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import { white, blue } from '../utils/colors'
 import { connect } from 'react-redux'
+import ImageButton from './ImageButton'
 
 class NewDeck extends Component {
     constructor(props) {
         super(props);
         this.state = { title : '' } ;
+        this.addNewDeck = this.addNewDeck.bind(this);
+    }
+
+    addNewDeck() {
 
     }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Name of Deck</Text>
+                <Text style={styles.titleLine}>Please enter the title of your new deck</Text>
                 <TextInput
                     numberOfLines={1}
                     maxLength={64}
@@ -20,6 +26,9 @@ class NewDeck extends Component {
                     style={styles.titleInput}
                     onChangeText={(text) => this.setState({text})}
                     value={this.state.text}/>
+                <ImageButton style={{ padding: 10 }} imageName='add-to-list' onPress={this.addNewDeck}>
+                    Add New Deck
+                </ImageButton>
             </View>
         )
     }
@@ -33,9 +42,18 @@ const styles = StyleSheet.create({
     },
     titleInput : {
         height: 40, 
-        borderColor: 'blue', 
+        borderColor: blue, 
         borderWidth: 1,
-        color : blue 
+        color : blue ,
+        paddingLeft : 10,
+        paddingRight : 10 , 
+        margin : 10,
+    },
+    titleLine : {
+        fontSize : 24,
+        margin : 20,
+        color : blue,
+
     }
 })
 
